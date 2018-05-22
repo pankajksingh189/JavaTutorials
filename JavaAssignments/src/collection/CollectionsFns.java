141fe9992162d6c7d49321e4976e9be93748978c
Pankaj Singh <pankajksingh189@gmail.com>
2018-05-23 01:16:39 +0530

    CollectionsFns

---
+++ JavaAssignments/src/collection/CollectionsFns.java
@@ -0,0 +1,58 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+/*
+-> Collections in java is a framework that provides an architecture to store and manipulate the group of objects.
+-> All the operations that you perform on a data such as searching, sorting, insertion, manipulation, deletion etc. can be performed by Java Collections.
+*/
+
+
+public class CollectionsFns {
+
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();//creating arraylist  
+		al.add("Ravi");//adding object in arraylist  
+		al.add("Vijay");  
+		al.add("Ravi");
+		al.add("Ajay");  
+		al.add("Pankaj");
+		al.add("Rahul");  
+		al.add("Sandeep");
+		al.add("Vijay");
+		
+		System.out.println("Using Iterator");
+		Iterator<String> itr=al.iterator();//getting Iterator from arraylist to traverse elements  
+		while(itr.hasNext()) 
+			System.out.println(itr.next());  
+
+		System.out.println("\nUsing Advance for loop");
+		for(String obj:al)  
+			System.out.println(obj);  
+
+		System.out.println("iterating the elements after removing the elements of al2...");  
+		ArrayList<String> al2=new ArrayList<String>();  
+		al2.add("Ravi");  
+		al2.add("Vijay");  
+		al.removeAll(al2); 
+		al.remove("Sandeep"); 
+		System.out.println();
+		Iterator<String> itr2=al.iterator();  
+		while(itr2.hasNext()){  
+			System.out.println(itr2.next());  
+		}  
+		
+		System.out.println("iterating the elements after retaining the elements of al2...");  
+		al2.add("Pankaj");  
+		al2.add("Rahul");
+		al2.add("Himanshu");
+		al.retainAll(al2);  
+		Iterator<String> itr3=al.iterator();  
+		while(itr3.hasNext()){  
+			System.out.println(itr3.next());  
+		}  
+		System.out.println("Checking if 'Pankaj' is present: " + al.contains("Pankaj"));
+	}
+}
---
+++ JavaAssignments/src/collection/set/LinkedHasSetDemo.java
@@ -0,0 +1,36 @@
+package collection.set;
+
+import java.util.LinkedHashSet;
+
+/*
+--> Contains unique elements only like LinkedHaslhset.
+--> Provides all optional set operations, and permits null elements.
+--> Maintains insertion order.
+ */
+
+public class LinkedHasSetDemo {
+
+	public static void main(String[] args) {
+		LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+		// Adding element to LinkedHaslhset
+		lhs.add("M");
+		lhs.add("B");
+		lhs.add("C");
+		lhs.add(null);
+		lhs.add("A");
+		lhs.add("M");
+		lhs.add("X");
+		lhs.add(null);
+
+		System.out.println("Size of LinkedHaslhset=" + lhs.size());
+		System.out.println("Original LinkedHaslhset:" + lhs);
+
+		System.out.println("Removing A from LinkedHaslhset: " + lhs.remove("A"));
+		System.out.println("Trying to Remove Z which is not present: "+ lhs.remove("Z"));
+		System.out.println("Checking if M is present=" + lhs.contains("M"));
+		System.out.println("Updated LinkedHaslhset: " + lhs);
+		System.out.println("\nUsing Advance for loop");
+		for(String value:lhs)  
+			System.out.println(value);  
+	}
+}
